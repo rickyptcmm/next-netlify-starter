d8ad16f2a5ff107c33602906261ff1b659ea5535
rickyptcmm <rickyptcmm@gmail.com>
2025-10-05 09:51:55 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export', // 👈 ini membuat static HTML
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
